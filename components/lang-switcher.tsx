e539e93537f0d825ed6e6c1e464818960cb162d9
wooqash <wooqash.webdesign@gmail.com>
2021-07-26 13:28:36 +0200

    feat: added some new code for lang switcher

--- components/lang-switcher.tsx
+++ components/lang-switcher.tsx
@@ -1,21 +1,34 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "./lang-switcher.module.scss";
+import { useCookies } from 'react-cookie';
+
+import { MouseEvent } from 'react';
 
 type LangSwitcherProps = {};
 
 const LangSwitcher: React.FC<LangSwitcherProps> = () => {
+  const [ cookie, setCookie ] = useCookies(['NEXT_LOCALE']);
   const router = useRouter();
+  const { locale, locales } = router;
+
+  const handleSwitchLang = (e: MouseEvent<HTMLAnchorElement>) => {
+    const target = e.target as HTMLAnchorElement;
+    const selectedLocale = target.hreflang;
+    if(cookie.NEXT_LOCALE !== selectedLocale){
+      setCookie("NEXT_LOCALE", selectedLocale, { path: "/" });
+    }
+  }
 
   return (
     <ul className={styles.langSwitcher}>
-      {router?.locales?.map((locale) => {
-        const activeElemClass = locale === router.locale ? styles['langSwitcher__elem--active'] : '';
-        const activeLinkClass = locale === router.locale ? styles['langSwitcher__link--active'] : '';
+      {locales?.map((loc) => {
+        const activeElemClass = loc === locale ? styles['langSwitcher__elem--active'] : '';
+        const activeLinkClass = loc === locale ? styles['langSwitcher__link--active'] : '';
         return (
-          <li key={locale} className={`${styles.langSwitcher__elem} ${activeElemClass}`}>
-            <Link href={router.asPath} locale={locale}>
-              <a className={`${styles.langSwitcher__link} ${activeLinkClass}`}>{locale.substr(0, 2)}</a>
+          <li key={loc} className={`${styles.langSwitcher__elem} ${activeElemClass}`}>
+            <Link href={router.asPath} locale={loc} passHref>
+              <a onClick={handleSwitchLang} className={`${styles.langSwitcher__link} ${activeLinkClass}`} hrefLang={loc}>{loc.substr(0, 2)}</a>
             </Link>
           </li>
         );
