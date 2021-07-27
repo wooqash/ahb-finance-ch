eccda6cf13d025c44a81110c05bbe1bbe926c0d1
wooqash <wooqash.webdesign@gmail.com>
2021-07-27 09:26:20 +0200

    style: refactor some global styles

--- types/coming-soon-page-data.ts
+++ types/coming-soon-page-data.ts
@@ -2,7 +2,6 @@ import { GlobalData } from "./global-data";
 import { NewsletterSubscribeDialogData } from "./newsletter-subscribe-dialog-data";
 import { SeoData } from "./seo-data";
 
-
 export type ComingSoonPageData = {
   comingSoon: {
     content: string;
