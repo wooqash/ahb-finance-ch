0fd5aa812add02316d1a49e148b0fc472c057105
wooqash <wooqash.webdesign@gmail.com>
2021-07-27 09:24:11 +0200

    feat: added link to the main logo

--- components/main-logo.tsx
+++ components/main-logo.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
+// import Link from 'next/link';
 import { ReactNode } from 'react';
 import { Media } from 'types/media';
+import { useRouter } from "next/router";
 
 type MainLogoProps = {
   logo: Media,
@@ -8,14 +10,19 @@ type MainLogoProps = {
 }
 
 const MainLogo:React.FC<MainLogoProps> = ({logo}) => {
+    const router = useRouter();
     return (
         <div className="mx-auto my-5 max w-4/5 md:w-full">
-          <Image
-            src={logo.url}
-            width={logo.width}
-            height={logo.height}
-            alt={logo.alternativeText}
-          />
+          {/* <Link href="/" locale={router.locale} passHref> */}
+            <a href={`/${router.locale}`}>
+              <Image
+                src={logo.url}
+                width={logo.width}
+                height={logo.height}
+                alt={logo.alternativeText}
+              />
+            </a>
+          {/* </Link> */}
         </div>
     );
 }
