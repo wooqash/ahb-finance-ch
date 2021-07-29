f8e477b27cceafb827f088e0b1faf49e4878db70
wooqash <wooqash.webdesign@gmail.com>
2021-07-29 13:06:33 +0200

    feat: added valid state and icons to newsletter form

--- components/newsletter-form.tsx
+++ components/newsletter-form.tsx
@@ -7,6 +7,8 @@ import { useRouter } from 'next/router';
 import React, { useEffect } from "react";
 import AriaLive from "./aria-live";
 
+import { MdDone, MdErrorOutline } from "react-icons/md";
+
 type NewsletterFormProps = {
   content: FormsData;
 };
@@ -79,30 +81,36 @@ const NewsletterForm: React.FC<NewsletterFormProps> = ({ content }) => {
                 onSubmit={handleSubmit}
                 className="lg:w-8/12 mx-auto flex flex-col text-left"
               >
-                <label htmlFor="name" className={`block mx-4 mt-4 ${(touched.name && errors.name ? `text-error-color` : `text-white`)}`}>
+                <label htmlFor="name" className={`block mx-4 mt-4 ${(touched.name && errors.name ? `text-error-color` : (touched.name && !errors.name ? `text-green-500` : `text-white`))}`}>
                   {nameLabel}
                 </label>
-                <Field
-                  id="name"
-                  name="name"
-                  placeholder={namePlaceholderMsg}
-                  className={`rounded bg-transparent border border-solid placeholder-gray-300 placeholder-opacity-50 text-center p-2 ${(touched.name && errors.name ? `border-error-color focus:border-error-color` : `border-white`)}`}
-                />
+                <div className="flex items-center relative">
+                  <Field
+                    id="name"
+                    name="name"
+                    placeholder={namePlaceholderMsg}
+                    className={`w-full rounded bg-transparent border border-solid placeholder-gray-300 placeholder-opacity-50 text-center p-2 pl-10 ${(touched.name && errors.name ? `border-error-color focus:border-error-color` : (touched.name && !errors.name ? `border-green-500 focus:border-green-500` : `border-white`))}`}
+                  />
+                  {touched.name && !errors.name && <MdDone className="absolute left-3 text-green-500" />} {touched.name && errors.name && <MdErrorOutline className="absolute left-3 text-error-color" />}
+                </div>
                 <ErrorMessage
                   component="span"
                   name="name"
                   className="block text-sm text-error-color mx-4 my-2"
                 />
-                <label htmlFor="email" className={`block mx-4 mt-4 ${(touched.email && errors.email ? `text-error-color` : `text-white`)}`}>
+                <label htmlFor="email" className={`block mx-4 mt-4 ${(touched.email && errors.email ? `text-error-color` : (touched.email && !errors.email ? `text-green-500` : `text-white`))}`}>
                   {emailLabel}
                 </label>
-                <Field
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder={emailPlaceholderMsg}
-                  className={`rounded bg-transparent border border-solid placeholder-gray-300 placeholder-opacity-50 text-center p-2 ${(touched.email && errors.email ? `border-error-color focus:border-error-color` : `border-white`)}`}
-                />
+                <div className="flex items-center relative">
+                  <Field
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder={emailPlaceholderMsg}
+                    className={`w-full rounded bg-transparent border border-solid placeholder-gray-300 placeholder-opacity-50 text-center p-2 pl-10 ${(touched.email && errors.email ? `border-error-color focus:border-error-color` : (touched.email && !errors.email ? `border-green-500 focus:border-green-500` : `border-white`))}`}
+                  />
+                  {touched.email && !errors.email && <MdDone className="absolute left-3 text-green-500" />} {touched.email && errors.email && <MdErrorOutline className="absolute left-3 text-error-color" />}
+                </div>
                 <ErrorMessage
                   component="span"
                   name="email"
