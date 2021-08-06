66e23c62ce38f25271f6c8cb212da6b377bad2b0
wooqash <wooqash.webdesign@gmail.com>
2021-08-06 18:04:28 +0200

    feat: addded 2 new types for handling cookie data

---
+++ types/cookie-groups-flags.ts
@@ -0,0 +1,8 @@
+export type CookieGroupsFlags = {
+    necessary: boolean;
+    preferences: boolean;
+    stats: boolean;
+    marketing: boolean;
+    social: boolean;
+    unclassified: boolean;
+}
---
+++ types/cookie-info-data.ts
@@ -0,0 +1,29 @@
+export enum CookieGroupTypes {
+    necessary = 'necessary',
+    preferences = 'preferences',
+    stats = 'stats',
+    marketing = 'marketing',
+    social = 'social',
+    unclassified = 'unclassified',
+}
+
+export type CookieInfoData = {
+    cookieBannerText: string;
+    settingsTitle: string;
+    settingsButtonLabel: string;
+    acceptAllCookiesButtonLabel: string;
+    acceptSelectedCookiesButtonLabel: string;
+    acceptNecessaryCookiesButtonLabel: string;
+    acceptButtonLabel: string;
+    tabs:  [{
+        id: number | string;
+        label: string;
+        description: string;
+    }]
+    groups: [{
+        id: number | string;
+        groupName: CookieGroupTypes;
+        title: string;
+        description: string;
+    }]
+}
