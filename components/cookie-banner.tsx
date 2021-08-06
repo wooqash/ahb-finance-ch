ab96986b71ab0706d9a057aaaa18dd718fbee63a
wooqash <wooqash.webdesign@gmail.com>
2021-08-06 18:05:36 +0200

    feat: added cookie banner component

---
+++ components/cookie-banner.tsx
@@ -0,0 +1,29 @@
+import Button from "@/components/button";
+import { CookieInfoData } from "types/cookie-info-data";
+import ReactMarkdown from "react-markdown";
+import rmStyles from "@/components/markdown-styles.module.scss";
+import { MouseEvent } from 'react';
+
+type CookieBannerProps = {
+    content: CookieInfoData;
+    onAcceptAllCookies?: () => void;
+    onActivateModal?: (e: MouseEvent) => void;
+}
+
+const CookieBanner: React.FC<CookieBannerProps> = ({content, onAcceptAllCookies, onActivateModal }) => {
+  const { cookieBannerText, settingsButtonLabel, acceptButtonLabel } = content;
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 z-50 p-5 bg-white text-primary">
+      <ReactMarkdown className={`${rmStyles.markdown}`}>{cookieBannerText}</ReactMarkdown>
+      <Button id="ConsentSettings" onClick={onActivateModal} className="mr-5">
+        {settingsButtonLabel}
+      </Button>
+      <Button id="ConsentAccept" onClick={onAcceptAllCookies} className="mr-5">
+        {acceptButtonLabel}
+      </Button>      
+    </div>
+  );
+};
+
+export default CookieBanner;
