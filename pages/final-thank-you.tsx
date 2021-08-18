f151f7d054cd0c3f92cd5c61812057aabd402cb3
wooqash <wooqash.webdesign@gmail.com>
2021-08-18 17:20:20 +0200

    feat: added back buttons to thank you pages

--- pages/final-thank-you.tsx
+++ pages/final-thank-you.tsx
@@ -1,14 +1,16 @@
 import { MouseEvent } from "react";
+import ReactMarkdown from "react-markdown";
 import { getFinalThankYouPageContent } from "@/lib/api";
 import { GetStaticProps } from "next";
 import { FinalThankYouPageData } from "types/final-thank-you-page-data";
-import ReactMarkdown from "react-markdown";
 import Head from "next/head";
+import { useRouter } from 'next/router';
 import Container from "@/components/container";
 import LayoutShort from "@/components/layout-short";
 import LangSwitcher from "@/components/lang-switcher";
 import MainLogo from "@/components/main-logo";
 import ShareButtons from "@/components/share-buttons";
+import Button from "@/components/button";
 
 type FinalThankYouProps = {
   content: FinalThankYouPageData;
@@ -19,10 +21,11 @@ type FinalThankYouProps = {
 const FinalThankYou: React.FC<FinalThankYouProps> = ({ content, preview, showCookiePolicy }) => {
   const { seo } = content?.finalThankYouPage;
   const mainContent = content?.finalThankYouPage?.content;
-  const { logo, siteName } = content?.global;
+  const { logo, siteName, backToMainPageButtonLabel } = content?.global;
   const metaTitle = seo?.metaTitle;
   const metaDescription = seo?.metaDescription;
   const mainLogo = logo && logo.length > 0 ? logo[0] : null;
+  const {locale} = useRouter();
 
   return (
     <>
@@ -46,6 +49,7 @@ const FinalThankYou: React.FC<FinalThankYouProps> = ({ content, preview, showCoo
           {mainLogo && <MainLogo logo={mainLogo} />}
           <div className="mx-auto my-10 lg:max-w-4xl">
             {mainContent && <ReactMarkdown>{mainContent}</ReactMarkdown>}
+            {backToMainPageButtonLabel && <a href={`/${locale}`}><Button className="my-4">{backToMainPageButtonLabel}</Button></a>}
           </div>
           {/* <ShareButtons /> */}
         </Container>
--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -1,14 +1,16 @@
 import { MouseEvent } from "react";
 import { getThankYouPageContent } from "@/lib/api";
 import { GetStaticProps } from "next";
+import Head from "next/head";
+import { useRouter } from 'next/router';
 import { ThankYouPageData } from "types/thank-you-page-data";
 import ReactMarkdown from "react-markdown";
-import Head from "next/head";
 import Container from "@/components/container";
 import LayoutShort from "@/components/layout-short";
 import LangSwitcher from "@/components/lang-switcher";
 import MainLogo from "@/components/main-logo";
 import ShareButtons from "@/components/share-buttons";
+import Button from "@/components/button";
 
 type ThankYouProps = {
   content: ThankYouPageData;
@@ -19,10 +21,11 @@ type ThankYouProps = {
 const ThankYou: React.FC<ThankYouProps> = ({ content, preview, showCookiePolicy }) => {
   const { seo } = content?.thankYouPage;
   const mainContent = content?.thankYouPage?.content;
-  const { logo, siteName } = content?.global;
+  const { logo, siteName, backToMainPageButtonLabel } = content?.global;
   const metaTitle = seo?.metaTitle;
   const metaDescription = seo?.metaDescription;
   const mainLogo = logo && logo.length > 0 ? logo[0] : null;
+  const {locale} = useRouter();
 
   return (
     <>
@@ -46,6 +49,7 @@ const ThankYou: React.FC<ThankYouProps> = ({ content, preview, showCookiePolicy 
           {mainLogo && <MainLogo logo={mainLogo} />}
           <div className="mx-auto my-10 lg:max-w-4xl">
             {mainContent && <ReactMarkdown>{mainContent}</ReactMarkdown>}
+            {backToMainPageButtonLabel && <a href={`/${locale}`}><Button className="my-4">{backToMainPageButtonLabel}</Button></a>}
           </div>
           {/* <ShareButtons /> */}
         </Container>
