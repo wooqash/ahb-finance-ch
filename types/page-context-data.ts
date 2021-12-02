ee42bd8d25f310a2d9129ffab77385748feb87ab
wooqash <wooqash.webdesign@gmail.com>
2021-12-02 17:03:41 +0100

    reafactor: extended page context data type attribute by Localization type

--- types/page-context-data.ts
+++ types/page-context-data.ts
@@ -1,15 +1,19 @@
-import { LocalizationPageData } from "./localization-page-data";
 import { LocalizedPathsData } from "./localized-paths-data";
 
-export type PageContextData = {
+export type Localization = {
+    id: number;
     locale: string;
-    locales: string[] | undefined;
-    defaultLocale: string | undefined;
-    slug: string | undefined;
-    slugs: { locale: string | undefined, slug: string | undefined }[] | undefined;
-    localizations: [{ locale: string; }];
 }
 
+export type PageContextData = {
+  locale: string;
+  locales: string[] | undefined;
+  defaultLocale: string | undefined;
+  slug: string | undefined;
+  slugs: { locale: string | undefined; slug: string | undefined }[] | undefined;
+  localizations: Localization[];
+};
+
 export interface ExtendedPageContextData extends PageContextData {
-    localizedPaths?: LocalizedPathsData[];
+  localizedPaths?: LocalizedPathsData[];
 }
