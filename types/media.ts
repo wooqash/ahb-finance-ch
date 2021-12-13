d3b314ba8a51224a5df8658da72cd2147c6114bc
wooqash <wooqash.webdesign@gmail.com>
2021-12-14 00:47:50 +0100

    refactor: moved image types to single file

--- types/media.ts
+++ types/media.ts
@@ -1,18 +1,23 @@
-import { ImageInfo } from "./image-info";
-
-export type Media = {
-    name: string;
-    alternativeText?: string;
-    caption?: string;
-    width: number;
+export interface ImageInfo {
+    ext: string;
+    hash: string;
     height: number;
     mime: string;
+    name: string;
+    size: number;
     url: string;
+    width: number;
+};
+
+export interface Media extends ImageInfo {
+    id: number | string;
+    alternativeText?: string;
+    caption?: string;
     formats: {
         large: ImageInfo;
         medium: ImageInfo;
         small: ImageInfo;
         thumbnail: ImageInfo;
     };
-    hash: string;
-}
+};
+
