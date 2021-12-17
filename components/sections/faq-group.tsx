d41039120660b93fc8559118a9c224db4109e47c
wooqash <wooqash.webdesign@gmail.com>
2021-12-18 00:07:57 +0100

    feat: created faq group component

--- components/sections/faq-group.tsx
+++ components/sections/faq-group.tsx
@@ -1,17 +1,53 @@
-import { SectionsData } from "types/sections-data";
+import { FaqGroupData } from "types/sections-data";
+import { getButtonAppearance } from "utils/button";
+import ButtonLink from "../links/button-link";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
 
 type FaqGroupProps = {
-    data: SectionsData;
-}
- 
-const FaqGroup:React.FC<FaqGroupProps> = (props) => {
-    const { data } = props;
-    return (
-        <div>
-            FaqGroup
-        </div>
-    );
-}
- 
- 
+  data: FaqGroupData;
+};
+
+const FaqGroup: React.FC<FaqGroupProps> = (props) => {
+  const { data } = props;
+  const { title, questionAndAnswer, moreBtn } = data;
+  return (
+    <section>
+      {title && <h2>{title}</h2>}
+      {questionAndAnswer ? (
+        <Accordion>
+          {questionAndAnswer.map((qaItem) => {
+              const { question, answer } = qaItem;
+            return (
+              <AccordionItem key={qaItem.id}>
+                <AccordionItemHeading>
+                  <AccordionItemButton>{question}</AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>
+                    {answer}
+                  </p>
+                </AccordionItemPanel>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      ) : (
+        ""
+      )}
+      {moreBtn && (
+        <ButtonLink
+          button={moreBtn}
+          appearance={moreBtn ? getButtonAppearance(moreBtn.type, "light") : ""}
+        />
+      )}
+    </section>
+  );
+};
+
 export default FaqGroup;
--- types/elements/faq-item-data.ts
+++ types/elements/faq-item-data.ts
@@ -1,4 +1,5 @@
 export type FaqItemData = {
+    id: number | string;
     question: string;
     answer: string;
 }
