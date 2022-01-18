d17474490be93f47699217eaf0f5d07174237f6f
wooqash <wooqash.webdesign@gmail.com>
2022-01-18 23:09:36 +0100

    feat: extended custom image comp. by the new default option named 'intrinsic'

--- components/elements/custom-image.tsx
+++ components/elements/custom-image.tsx
@@ -3,24 +3,35 @@ import { getStrapiMedia } from "utils/media";
 import Image, { ImageLoaderProps } from "next/image";
 
 type CustomImageProps = {
-  media: Pick<Media, "url" | "width" | "height" | "alternativeText">;
-  layout?: "intrinsic" | "fixed" | "fill" | "responsive" | undefined;
-  isBgImage?: boolean; 
+  media: Pick<Media, "url" | "width" | "height" | "alternativeText">,
+  layout?: "intrinsic" | "fixed" | "fill" | "responsive" | undefined,
+  isBgImage?: boolean,
+  quality?: number,
+  cWidth?: number,
+  cHeight?: number
 };
 
 const CustomImage: React.FC<CustomImageProps> = (props) => {
-  const { media, layout = "intrinsic", isBgImage } = props;
-  const { url, alternativeText, width, height } = media;
+  const { media, layout = "intrinsic", isBgImage, cWidth, cHeight } = props;
+  const { url, alternativeText} = media;
+  const width = cWidth || media.width;
+  const height = cHeight || media.height;
 
   const loader = (prop: ImageLoaderProps) => {
     const url = getStrapiMedia(prop.src) || "";
     return url;
   };
 
-  // The image has a fixed width and height
-  if (width && height) {
+  if (layout === "intrinsic") {
     return (
       <Image loader={loader} src={url} alt={alternativeText || ""} {...media} />
+    );
+  }
+  
+  // The image has a fixed width and height
+  if (width && height && layout === "fixed") {
+    return (
+      <Image loader={loader} src={url} alt={alternativeText || ""} layout="fixed" width={width} height={height} />
     );
   }
 
