fcdd9a6669fcc1b68d1151164270b9262e79e94b
wooqash <wooqash.webdesign@gmail.com>
2022-01-18 23:07:53 +0100

    feat: updated modal newsletter by new image custom component

--- components/Modal/contents/modal-newsletter.tsx
+++ components/Modal/contents/modal-newsletter.tsx
@@ -3,11 +3,11 @@ import ReactMarkdown from "react-markdown";
 import { Media } from "types/media";
 import { FormsData } from "types/forms-data";
 
-import MainLogo from "@/components/main-logo";
 import NewsletterForm from "@/components/newsletter-form";
 
 import rmStyles from '@/components/markdown-styles.module.scss';
 import { NewsletterModalInfoData } from "types/newsletter-modal-info-data";
+import CustomImage from "@/components/elements/custom-image";
 
 type ModalNewsletterProps = {
     content: NewsletterModalInfoData;
@@ -20,7 +20,7 @@ const ModalNewsletter:React.FC<ModalNewsletterProps> = ({logo, form, content, on
 
     return (
         <>
-            {logo && <MainLogo logo={logo} />}
+            {logo && <CustomImage media={logo} />}
               <div className="lg:w-9/12 mx-auto">
                   {content.offerSummary && <ReactMarkdown className={`${rmStyles.markdown}`}>{content.offerSummary}</ReactMarkdown>}
                   {content.confirmationReminder && <ReactMarkdown className={`${rmStyles.markdown}`}>{content.confirmationReminder}</ReactMarkdown>}
