a0dde321a1835b740c69bb233840e014990f9a98
wooqash <wooqash.webdesign@gmail.com>
2022-01-18 23:18:00 +0100

    feat: updated navbar component and added main section at layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,22 +2,27 @@ import { ReactNode } from 'react';
 import { ExtendedPageContextData } from 'types/page-context-data';
 
 import { GlobalData } from 'types/global-data';
-import Navbar from './layout/navbar';
+
 import Footer from './layout/footer';
+import { PageAppearance } from 'types/page-appearance.enum';
+import { DEFAULT_LABELS } from 'utils/default-labels';
+import Navbar from './top-bar/navbar';
 
 type LayoutProps = {
     children?: ReactNode;
     global: GlobalData;
     pageContext: ExtendedPageContextData;
+    appearance: PageAppearance;
 }
 
-const Layout:React.FC<LayoutProps> = ({ children, global, pageContext }) => {
-    const { mainNav, footer, socialMediaLinks } = global;
+const Layout:React.FC<LayoutProps> = (props) => {
+    const { children, global, pageContext, appearance } = props;
+    const { mainNav, footer, socialMediaLinks, skipToMainContentLabel = DEFAULT_LABELS.skipToMainContentLabel } = global;
 
     return (
         <>
-            <Navbar navbar={mainNav} socialMedia={socialMediaLinks} pageContext={pageContext} />
-            <div>{children}</div>
+            <Navbar navbar={mainNav} socialMedia={socialMediaLinks} skipLabel={skipToMainContentLabel} pageContext={pageContext} appearance={appearance} />
+            <main id="Main">{children}</main>
             <Footer footer={footer} socialMedia={socialMediaLinks} pageContext={pageContext} />
         </>
     )
