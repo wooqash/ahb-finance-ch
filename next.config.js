1116696321852d7cdea4d908e10d56a223a77a75
wooqash <wooqash.webdesign@gmail.com>
2022-02-10 23:34:10 +0100

    feat: next.config file after prettyfing

--- next.config.js
+++ next.config.js
@@ -1,46 +1,45 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    i18n: {
-      locales: ['en', 'de-CH', 'pl'],
-      defaultLocale: 'de-CH',
-      localeDetection: false,
-    },
-    images: {
-      // loader: 'cloudinary',
-      // path: 'https://res.cloudinary.com/zwds/image/upload/v1624333815/',
-      domains: ['res.cloudinary.com']
-    },
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-    },
-    // async rewrites() {
-    //   return [
-    //     {
-    //       source: '/pl/coming-soon',
-    //       destination: '/pl/strona-w-budowie',
-    //       locale: false
-    //     },
-    //     {
-    //       source: '/coming-soon',
-    //       destination: '/kommt-bald',
-    //       locale: false
-    //     },
-    //     {
-    //       source: '/en/coming-soon',
-    //       destination: '/en/coming-soon',
-    //       locale: false
-    //     },
-    //   ]
-    // },
-    // async redirects() {
-    //   return [
-    //     {
-    //       source: '/',
-    //       destination: '/coming-soon',
-    //       permanent: true,
-    //     },
-    //   ]
-    // },
-    
-}
+  i18n: {
+    locales: ["en", "de-CH", "pl"],
+    defaultLocale: "de-CH",
+    localeDetection: false,
+  },
+  images: {
+    // loader: 'cloudinary',
+    // path: 'https://res.cloudinary.com/zwds/image/upload/v1624333815/',
+    domains: ["res.cloudinary.com"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/pl/coming-soon',
+  //       destination: '/pl/strona-w-budowie',
+  //       locale: false
+  //     },
+  //     {
+  //       source: '/coming-soon',
+  //       destination: '/kommt-bald',
+  //       locale: false
+  //     },
+  //     {
+  //       source: '/en/coming-soon',
+  //       destination: '/en/coming-soon',
+  //       locale: false
+  //     },
+  //   ]
+  // },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/coming-soon',
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
+};
