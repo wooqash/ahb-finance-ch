6c77d453a1176b58cbcf778b326dc1e69a6382b9
wooqash <wooqash.webdesign@gmail.com>
2022-02-10 23:31:33 +0100

    feat: added component structure to the hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -2,25 +2,53 @@ import { HeroData } from "types/sections-data";
 import { getButtonAppearance } from "utils/button";
 import ButtonLink from "../links/button-link";
 
+import style from "./hero.module.scss";
+
+import { HERO_VIDEO } from "lib/constants";
+import Video from "../elements/video";
+import Image from 'next/image';
+
+import Watermark from 'public/images/wartermark-pig.svg';
+import ScrollDownArrow from 'public/icons/arrow-down.svg';
 
 type HeroProps = {
-    data: HeroData;
-}
- 
-const Hero:React.FC<HeroProps> = (props) => {
-    const { data } = props;
-    return (
-        <section>
-            <h1>{data.title}</h1>
+  data: HeroData;
+  pageName: string;
+};
+
+const Hero: React.FC<HeroProps> = (props) => {
+  const { data, pageName } = props;
+
+  return (
+    <section className={`${style.section} ${style["section--" + pageName]}`}>
+      {pageName === "home" && <Video videoSrc={HERO_VIDEO} />}
+      <div className={style.section__wrapper}>
+        <div className={style.section__caption}>
+          <h1 className={style.section__title}>{data.title}</h1>
+          <div className={style["section__buttons-wrapper"]}>
             {data.cta?.map((button) => {
-                return <ButtonLink
-                    button={button}
-                    appearance={getButtonAppearance(button.type, "light")}
-                    key={button.id}
+              return (
+                <ButtonLink
+                  button={button}
+                  appearance={getButtonAppearance(button.type, "dark")}
+                    // appearance={button.type}
+                  key={button.id}
                 />
+              );
             })}
-        </section>
-    );
-}
- 
+          </div>
+        </div>
+        {pageName === "home" && (
+          <div className={style.section__watermark}>
+            <Image src={Watermark} alt={`transparent pig watermark`} />
+          </div>
+        )}
+        <div className={style["scroll-down-icon"]}>
+            <Image src={ScrollDownArrow} alt={`scroll down image`} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Hero;
