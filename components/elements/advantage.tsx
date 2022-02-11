03ebb553ff9923f02d103b76e9ac9db8931d2dec
wooqash <wooqash.webdesign@gmail.com>
2022-02-11 18:48:20 +0100

    feat: create structure and styles for advantages section

--- components/elements/advantage.tsx
+++ components/elements/advantage.tsx
@@ -1,6 +1,8 @@
 import { AdvantageData } from "types/elements/advantage-data";
 import CustomImage from "./custom-image";
 
+import style from "./advantage.module.scss";
+
 type AdvantageProps = {
     advantage: AdvantageData;
 }
@@ -10,10 +12,10 @@ const Advantage:React.FC<AdvantageProps> = (props) => {
     const { title, description, icon } = advantage;
 
     return (
-        <div>
-            <h3>{title}</h3>
-            {icon &&<CustomImage media={icon} />}
-            <p>{description}</p>
+        <div className={style.column}>
+            {icon && <div className={style.column__icon}><CustomImage media={icon} /></div>}
+            <h3 className={style.column__title}>{title}</h3>
+            <p className={style.column__text}>{description}</p>
         </div>
     );
 }
--- components/sections/advantages-group.tsx
+++ components/sections/advantages-group.tsx
@@ -1,23 +1,37 @@
+import Image from 'next/image';
+
 import { AdvantagesGroupData } from "types/sections-data";
 import Advantage from "../elements/advantage";
 
+import style from "./advantages-group.module.scss";
+import TopTriangle from 'public/images/adv-top-triangle.svg';
+import BottomTriangle from 'public/images/adv-bottom-triangle.svg';
 
 type AdvantagesGroupProps = {
-    data: AdvantagesGroupData;
-    // children: React.ReactChild;
-}
- 
-const AdvantagesGroup:React.FC<AdvantagesGroupProps> = (props) => {
-    const { data } = props;
-    return (
-        <section>
-            <h2>{data.title}</h2>
-            {data.advantages.map((advantage)=> {
-                return <Advantage advantage={advantage} key={advantage.id} />
-            })}
-        </section>
-    );
-}
- 
- 
+  data: AdvantagesGroupData;
+  // children: React.ReactChild;
+};
+
+const AdvantagesGroup: React.FC<AdvantagesGroupProps> = (props) => {
+  const { data } = props;
+  return (
+    <section className={style.section}>
+      <div className={style.section__wrapper}>
+        <h2 className={style.section__title}>{data.title}</h2>
+        <div className={style.section__columns}>
+          {data.advantages.map((advantage) => (
+            <Advantage advantage={advantage} key={advantage.id} />
+          ))}
+        </div>
+      </div>
+      <div className={style["section__top-triangle"]}>
+        <Image src={TopTriangle} alt="" />
+      </div>
+      <div className={style["section__bottom-triangle"]}>
+        <Image src={BottomTriangle} alt="" />
+      </div>
+    </section>
+  );
+};
+
 export default AdvantagesGroup;
