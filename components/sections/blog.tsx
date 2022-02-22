494481f28bfae66b754f9338fe627108d40735a1
wooqash <wooqash.webdesign@gmail.com>
2022-02-23 00:32:06 +0100

    feat: changed the html structure in blog component

--- components/sections/blog.tsx
+++ components/sections/blog.tsx
@@ -1,32 +1,52 @@
 import { ArticleData } from "types/blog-data";
-import { ButtonLinkData } from "types/buttons-data";
 import { BlogData } from "types/sections-data";
 import { getButtonAppearance } from "utils/button";
 import Card from "../elements/card";
+import BlogSlider from "../elements/blog-slider";
 import ButtonLink from "../links/button-link";
+import useDeviceDetect from "@/lib/useDeviceDetect";
+
+import style from "./blog.module.scss";
 
 type BlogProps = {
-    data: BlogData;
-    articles?: ArticleData[];
-}
- 
-const Blog:React.FC<BlogProps> 
-= (props) => {
-    const { data, articles } = props;
-    const { title, moreBtn } = data;
+  data: BlogData;
+  articles?: ArticleData[];
+};
 
-    return (
-        <section>
-            <h2>{title}</h2>
-            {moreBtn && <ButtonLink
-                button={moreBtn}
-                appearance={moreBtn ? getButtonAppearance(moreBtn.type, "light") : ""}
-            />}
-            {articles?.map((article) => {
-                return (<Card article={article} key={article.id} />);
-            })}
-        </section>
-    );
+const Blog: React.FC<BlogProps> = (props) => {
+  const { data, articles } = props;
+  const { title, moreBtn } = data;
+  const isMobile = useDeviceDetect();
+
+  return (
+    <section className={`blog ${style.section}`}>
+      <div className={style.section__wrapper}>
+        <h2 className={style.section__title}>{title}</h2>
+        <div className={style.section__readMoreBtn}>
+          {moreBtn && (
+            <ButtonLink
+              button={moreBtn}
+              appearance={
+                moreBtn ? getButtonAppearance(moreBtn.type, "dark") : ""
+              }
+            />
+          )}
+        </div>
+        {articles && (
+          <>
+            {isMobile && (
+              <div className={style.section__blogCards}>
+                {articles.map((article) => (
+                  <Card article={article} key={article.id} />
+                ))}
+              </div>
+            )}
+            {!isMobile && <BlogSlider data={articles} />}
+          </>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
