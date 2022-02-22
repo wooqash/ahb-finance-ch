9c3a7f761b71aa113d2ff6e3c9a9d4bc6e7447c4
wooqash <wooqash.webdesign@gmail.com>
2022-02-23 00:29:56 +0100

    feat: changed the card components

--- components/elements/card.tsx
+++ components/elements/card.tsx
@@ -5,33 +5,37 @@ import { getButtonAppearance } from "utils/button";
 import ButtonLink from "../links/button-link";
 import CustomImage from "./custom-image";
 
+import style from "./card.module.scss";
+import CustomLink from "../links/custom-link";
+
 type CardProps = {
   article: ArticleData;
 };
 
 const Card: React.FC<CardProps> = (props) => {
   const { article } = props;
-  const { image, title, description } = article;
-  const button = {
+  const { image, title } = article;
+  const link = {
     id: article.id,
-    label: "read",
-    type: ButtonType.secondary,
+    label: "",
     url: `/article/${article.slug}`,
+    newTab: false,
   };
 
   return (
-    <div>
-      <div>
-        <CustomImage media={image} />
-      </div>
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <ButtonLink
-          button={button}
-          appearance={getButtonAppearance(button.type, "light")}
-        />
-      </div>
+    <div className={style.item}>
+      <CustomLink
+        id={`ArticleLink${article.id}`}
+        link={link}
+        className={style.item__link}
+      >
+        <div>
+          <div className={style.item__image}>
+            <CustomImage media={image} layout="fill" />
+          </div>
+          <h3 className={style.item__title}>{title}</h3>
+        </div>
+      </CustomLink>
     </div>
   );
 };
