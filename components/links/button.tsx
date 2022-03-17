2dbde9e70a082b3f7cc0f525956d8049a9d9227b
wooqash <wooqash.webdesign@gmail.com>
2022-03-17 23:49:09 +0100

    feat: added type attribute to button component

--- components/links/button.tsx
+++ components/links/button.tsx
@@ -7,13 +7,14 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 type ButtonProps = {
     button: ButtonData;
     appearance: string;
+    type?: "button" | "submit" | "reset" | undefined;
     handleClick?: (e: MouseEvent<HTMLButtonElement>) => void;
 }
  
 const Button:React.FC<ButtonProps> = (props) => {
-    const { button, appearance, handleClick } = props;
+    const { button, appearance, type = "button", handleClick } = props;
     return (
-        <button onClick={handleClick} className={`${style.button} ${style[`button--${appearance}`]}`}>
+        <button onClick={handleClick} className={`${style.button} ${style[`button--${appearance}`]}`} type={type}>
             <span className={style["button__content-wrapper"]}>
                 <span className={style.button__caption}>{button.label}</span>
                 <span className={style.button__icon}><HiArrowNarrowRight /></span>
